2f82bb5926c41c4d67b2575fefc7406195b1ff1e
Precious Adedokun <apreezofficial@gmail.com>
2025-06-06 01:30:51 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,3 @@
+  <div class="border-t border-gray-700 mt-12 pt-6 text-center text-xs text-gray-900 dark-text-gray-700 select-none">
+  Made with love By <a href="https://preciousadedokun.com.ng" class="underline text-blue-700 dark-text-blue-200">Precious Adedokun</a> &copy; <?php echo date('Y'); ?>
+  </div>

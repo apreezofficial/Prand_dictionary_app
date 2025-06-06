eaa029e0d3ece5ab84bdc2833c10cb014e0a26b0
Precious Adedokun <apreezofficial@gmail.com>
2025-06-06 01:31:13 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'nav.php';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title><?php echo $app_name; ?></title>
+  <title><?php echo $app_name; ?> By Precious</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -70,5 +70,6 @@ include 'nav.php';
       animation: shimmer 10s infinite ease-in-out;
     }
   </style>
+  <?php include 'footer.php';?>
   <?php include 'script.php'; ?>
 </html>
